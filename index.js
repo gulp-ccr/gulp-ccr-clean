af1ff9f3d84b58ce71187d67567ff69664209d0e
amobiz <amobiz.tw+github@gmail.com>
2016-01-10 23:05:20 +0800

    update schema and options

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ function cleanTask() {
 	// lazy loading required modules.
 	var del = require('del');
 
-	return del(this.config.dest.path);
+	return del(this.config.dest.path, this.config.options);
 }
 
 cleanTask.schema = {
@@ -20,7 +20,23 @@ cleanTask.schema = {
 	description: '',
 	properties: {
 		dest: {
-			description: ''
+			description: 'The output path to clean.',
+			type: 'path'
+		},
+		options: {
+			type: 'object',
+			properties: {
+				force: {
+					description: 'Allow deleting the current working directory and files/folders outside it.',
+					type: 'boolean',
+					default: false
+				},
+				dryRun: {
+					description: 'See what would be deleted.',
+					type: 'boolean',
+					default: false
+				}
+			}
 		}
 	},
 	required: ['dest']
